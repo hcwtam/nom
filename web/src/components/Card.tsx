f0453ff93b7323c3a9557f05c6d1a89f32eec5c9
Wesley Tam <hcwtam@gmail.com>
2021-02-03 23:41:37 +0800

    create card component

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,5 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text, Badge } from '@chakra-ui/react';
 
 export const Card = () => (
-  <Box spacing="1.5rem" maxWidth="48rem" pt="8rem" px="1rem"></Box>
+  <Box
+    spacing="1.5rem"
+    maxWidth="10rem"
+    p="1rem"
+    borderRadius="2xl"
+    bg="gray.800"
+  >
+    <Text as="h3" fontSize="2xl" fontWeight="semibold">
+      Boiled egg
+    </Text>
+    <Text>Boiled egg</Text>
+    <Badge>10 mins</Badge>
+  </Box>
 );
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { Flex } from '@chakra-ui/react';
+import { Card } from '../components/Card';
 import { Container } from '../components/Container';
 import { Main } from '../components/Main';
 
 const Index = () => (
   <Container height="100vh">
-    <Main></Main>
+    <Main>
+      <Flex align="center" justify="space-between" w="100%">
+        <Card />
+        <Card />
+        <Card />
+      </Flex>
+    </Main>
   </Container>
 );
 
