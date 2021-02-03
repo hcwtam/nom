7e0de228e39baa70f691c8a38d03068f2097a9e6
Wesley Tam <hcwtam@gmail.com>
2021-02-03 21:09:21 +0800

    basics chakra setup

---
+++ web/src/components/Card.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@chakra-ui/react';
+
+export const Card = () => (
+  <Box spacing="1.5rem" maxWidth="48rem" pt="8rem" px="1rem"></Box>
+);
--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -1,19 +1,14 @@
-import { Flex, useColorMode, FlexProps } from '@chakra-ui/react'
+import { Flex, FlexProps } from '@chakra-ui/react';
 
 export const Container = (props: FlexProps) => {
-  const { colorMode } = useColorMode()
-
-  const bgColor = { light: 'gray.50', dark: 'gray.900' }
-
-  const color = { light: 'black', dark: 'white' }
   return (
     <Flex
       direction="column"
       alignItems="center"
       justifyContent="flex-start"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
+      bg="gray.900"
+      color="white"
       {...props}
     />
-  )
-}
+  );
+};
